ad16f2a36f9047849a18fc26fdca7e098dfe613f
Michael Greco <michael.greco07@gmail.com>
2020-04-29 20:18:32 -0400

    render data

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import { Button, Input } from '@material-ui/core';
 function App() {
   const [songs, setSongs] = useState([]);
   const [toggle, setToggle] = useState(true);
-  const [query, setQuery] = useState(['nickleback']);
+  const [query, setQuery] = useState(['drake']);
 
   useEffect(() => {
     async function getData() {
       const response = await axios.get(
-        'https://itunes.apple.com/search?term=nickleback'
+        'https://itunes.apple.com/search?term=drake'
       );
       setSongs(response.data.results);
       console.log(songs);
     }
     getData();
   }, [toggle]);
-
+  console.log(songs);
   return (
     <div className="App">
       <header className="App-header">
         <Input onChange={(event) => setQuery(event.target.value)}></Input>
         <Button onClick={() => setToggle(!toggle)}>Search</Button>
+
+        <div>
+          {songs.map((el) => (
+            <div key={el.trackId}>{el.trackName}</div>
+          ))}
+        </div>
       </header>
     </div>
   );
