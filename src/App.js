72826b06c76791499ac0f797dc9f526044b5cd1c
Michael Greco <michael.greco07@gmail.com>
2020-04-29 20:11:39 -0400

    setState, input & button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button, Input } from '@material-ui/core';
 
 function App() {
+  const [songs, setSongs] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  const [query, setQuery] = useState(['nickleback']);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(
+        'https://itunes.apple.com/search?term=nickleback'
+      );
+      setSongs(response.data.results);
+      console.log(songs);
+    }
+    getData();
+  }, [toggle]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello</h1>
+        <Input onChange={(event) => setQuery(event.target.value)}></Input>
+        <Button onClick={() => setToggle(!toggle)}>Search</Button>
       </header>
     </div>
   );
